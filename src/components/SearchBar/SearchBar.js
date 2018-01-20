520322f83541837a4803d73bd8487782ab981b99
Ryan Kelso <ryan.kelso@lewiscountywa.gov>
2018-01-20 09:57:31 -0800

    Add submit search term action

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,11 +2,31 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchTerm: ''}
+    this.handleClickSearch = this.handleClickSearch.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  handleClickSearch() {
+    // pass the value of the search term to this.props.onSearch()
+    this.props.onSearch(this.state.searchTerm);
+  }
+
+  handleTermChange(event) {
+    // set a search term variable to the event (search box) target's value
+    this.setState({searchTerm: event.target.value});
+  }
+
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
-        <a>SEARCH</a>
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+        {/* Added onClick to make submitting a search term work, it doesn't
+        seem to be addressed in the instructions? Steps 66-73. Is it addressed
+        in a later step??*/}
+        <a onClick={this.handleClickSearch}>SEARCH</a>
       </div>
     );
   }
