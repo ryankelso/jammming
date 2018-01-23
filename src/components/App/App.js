22749f47bb0a2c2a4d17c29160187c81cffac4f0
Ryan Kelso <RPKelso@lewis.local>
2018-01-22 16:21:21 -0800

    continue Spotify getAccessToken method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SearchBar from './../SearchBar/SearchBar.js';
 import SearchResults from './../SearchResults/SearchResults.js';
 import Playlist from './../Playlist/Playlist.js';
+import Spotify from './../../util/Spotify.js';
 
 // Hard code a static list of tracks for building the basic structure of the app
 const track1 = {
@@ -118,6 +119,7 @@ class App extends React.Component {
 
   search(searchTerm) {
     console.log(searchTerm);
+    Spotify.getAccessToken();
   }
 
   render() {
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,8 +1,27 @@
 let accessToken = '';
+const clientId = 'ac9e81ffc1724c7b82eeca5d92c3c994';
+const redirectUri = 'http://localhost:3000/';
 
 const Spotify = {
-  getAccessToken() {
-
+  async getAccessToken() {
+    if (accessToken) {
+      return accessToken
+    }
+    else {
+      try {
+        let response = await fetch(`https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`);
+        if (response.ok) {
+          let jsonResponse = await response.json();
+          // do something with jsonResponse...
+          // console.log('jsonResponse');
+          return jsonResponse;
+        }
+        throw new Error('Request Failed!');
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
   }
 };
 
