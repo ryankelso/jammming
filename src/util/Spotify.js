587ee927d01c9fe18e3ef38f1d8abcd79b2756fc
Ryan Kelso <ryan.kelso@lewiscountywa.gov>
2018-01-22 20:52:21 -0800

    Continue working on getAccessToken method

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,26 +1,37 @@
 let accessToken = '';
+let expiresIn
 const clientId = 'ac9e81ffc1724c7b82eeca5d92c3c994';
 const redirectUri = 'http://localhost:3000/';
 
 const Spotify = {
-  async getAccessToken() {
+  getAccessToken() {
     if (accessToken) {
       return accessToken
     }
     else {
-      try {
-        let response = await fetch(`https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`);
-        if (response.ok) {
-          let jsonResponse = await response.json();
-          // do something with jsonResponse...
-          // console.log('jsonResponse');
-          return jsonResponse;
-        }
-        throw new Error('Request Failed!');
+      // Parse the URL to see if it contains the just-aquired token,
+      // but it's not set to the variable yet
+      accessToken = window.location.href.match(/access_token=([^&]*)/);
+      expiresIn = window.location.href.match(/expires_in=([^&]*)/);
+
+      if (accessToken) {
+        //  these lines for resetting the url should happen after the conditional statements evaluate?
+        window.setTimeout(() => accessToken = '', expiresIn * 1000);
+        window.history.pushState('Access Token', null, '/');
+        console.log(accessToken);
+        return accessToken
       }
-      catch (error) {
-        console.log(error);
+
+      // Finally, if the access token variable is not set, and is not
+      // in the URL, then redirect the user to the spotify authorize
+      // URL to get a login and get a token
+      else {
+        window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
       }
+
+
+
+
     }
   }
 };
